7c0432edc404e3ecb32ecc0b08d9c5427a4c0fad
FirstshortAtm <mnajmuddin110@gmail.com>
2025-09-28 02:53:26 +0500

    Add to files

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>Me aur React | First print by Muhammad Najmuddin in ReactJs </h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+
+  return (
+   <h1>Me aur React | Muhammad Najmuddin</h1>
+  )
+}
+
+export default App
